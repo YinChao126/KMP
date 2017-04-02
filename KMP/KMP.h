4454d34eb1d8886818bccc107df1a2ad5b80cbed
Yin Chao <yc86247931@126.com>
2017-04-02 09:23:46 +0800

    创建一个完整可测版本 KMP算法
    包含了C和C++两个版本的代码。通过头文件进行切换

--- KMP/KMP.h
+++ KMP/KMP.h
@@ -1,5 +1,18 @@
 #ifndef _KMP_H_
 #define _KMP_H_
 
+//#define Cplusplus
 
+
+#ifdef Cplusplus
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+int kmp_find(const string& target, const string& pattern);
+#else
+#include <string.h>
+#include <stdlib.h>
+int kmp_find(const char *target, const char *pattern);
+#endif
 #endif
